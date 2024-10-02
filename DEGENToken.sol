c19e3424fa5089b86fddc9bd3ea47714d4bc9a87
ashish0719 <130694720+ashish0719@users.noreply.github.com>
2024-10-02 12:40:26 +0530

    Update DEGENToken.sol

--- DEGENToken.sol
+++ DEGENToken.sol
@@ -1,4 +1,3 @@
-
 pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts@5.0.2/token/ERC20/ERC20.sol";
@@ -6,51 +5,74 @@ import "@openzeppelin/contracts@5.0.2/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts@5.0.2/access/Ownable.sol";
 
 contract DegenToken is ERC20, ERC20Burnable, Ownable {
-    // Arrays for storing food items and their prices
-    string[] public foodItems;
-    uint256[] public foodPrices;
+    // Arrays for storing redeemable items and their token prices
+    string[] public items;
+    uint256[] public itemPrices;
 
-    constructor(address initialOwner)
-        ERC20("DegenToken", "DGN")
-        Ownable(initialOwner)
+    // Mapping to store redeemed items by user
+    mapping(address => string[]) public redeemedItems;
+
+    constructor(address initialOwner) 
+        ERC20("DegenToken", "DGN") 
+        Ownable(initialOwner) 
     {
-        
-        foodItems.push("Pizza Hut Pizza");
-        foodItems.push("Domino's Pizza");
-        foodItems.push("Subway Sandwich");
-        foodItems.push("Starbucks Coffee");
+        // Predefined Warzone-style items
+        items.push("M4 Assault Rifle");        // Item 1
+        items.push("AK-47");                   // Item 2
+        items.push("Sniper Rifle");            // Item 3
+        items.push("Bulletproof Vest");        // Item 4
+        items.push("Grenades");                // Item 5
+        items.push("Tactical Helmet");         // Item 6
+        items.push("Medkit");                  // Item 7
+        items.push("Ammo Pack");               // Item 8
+        items.push("Drone");                   // Item 9
+        items.push("Armored Vehicle");         // Item 10
 
-        foodPrices.push(50); 
-        foodPrices.push(60); 
-        foodPrices.push(40); 
-        foodPrices.push(30); 
+        // Corresponding token prices for items
+        itemPrices.push(200);  // M4 Assault Rifle
+        itemPrices.push(180);  // AK-47
+        itemPrices.push(250);  // Sniper Rifle
+        itemPrices.push(150);  // Bulletproof Vest
+        itemPrices.push(50);   // Grenades
+        itemPrices.push(100);  // Tactical Helmet
+        itemPrices.push(120);  // Medkit
+        itemPrices.push(60);   // Ammo Pack
+        itemPrices.push(300);  // Drone
+        itemPrices.push(500);  // Armored Vehicle
     }
 
-    // Function to redeem a food item based on itemId
+    
     function redeem(uint256 itemId) public {
-       
-        require(itemId < foodItems.length, "Item does not exist");
+        require(itemId < items.length, "Item does not exist");
 
         // Ensure the sender has enough tokens to redeem the item
-        uint256 price = foodPrices[itemId];
+        uint256 price = itemPrices[itemId];
         require(balanceOf(msg.sender) >= price, "Insufficient balance");
 
-        
+        // Burn tokens from the user's account
         _burn(msg.sender, price);
 
-        
-        emit Redeemed(msg.sender, foodItems[itemId], price);
+        // Add the redeemed item to the user's list
+        redeemedItems[msg.sender].push(items[itemId]);
+
+        // Emit an event to log the redemption
+        emit Redeemed(msg.sender, items[itemId], price);
     }
 
-    // Event to log the redemption of items
+    // Event to log item redemption
     event Redeemed(address indexed user, string item, uint256 amount);
 
-    // Function to mint new tokens
+    // Function to mint new tokens (Only owner can mint)
     function mint(address to, uint256 amount) public onlyOwner {
         _mint(to, amount);
     }
 
-    
+    // Function to get the list of redeemed items for a specific user
+    function getRedeemedItems(address user) public view returns (string[] memory) {
+        return redeemedItems[user];
+    }
+
+    // Override the decimals function to return 0, ensuring tokens are whole numbers
     function decimals() public pure override returns (uint8) {
         return 0; // 100 tokens will be equal to 100 tokens only
     }
@@ -60,13 +82,7 @@ contract DegenToken is ERC20, ERC20Burnable, Ownable {
         return balanceOf(msg.sender);
     }
 
-    // Get a food item by index
-    function getFoodItem(uint256 index) public view returns (string memory) {
-        require(index < foodItems.length, "Item index out of bounds");
-        return foodItems[index];
-    }
-
-    
+    // Custom transfer function with extra balance check
     function transfer(address recipient, uint256 amount) public override returns (bool) {
         require(balanceOf(msg.sender) >= amount, "You do not have enough Degen Tokens");
         _transfer(msg.sender, recipient, amount);
